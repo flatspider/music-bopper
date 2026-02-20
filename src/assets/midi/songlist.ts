e3192fe94185541ee0dcbdf48ad0cb26be5efdf2
Josh U <joshupad@gmail.com>
2026-02-20 14:11:55 -0500

    added new tracks, full MIDI is played but separated by Tones

--- src/assets/midi/songlist.ts
+++ src/assets/midi/songlist.ts
@@ -1,10 +1,14 @@
 import type { MidiSongJson } from "../../types/miditypes";
 import ColtraneData from "./json/COLTRANE.Countdown.json";
-import PiratesData from "./json/Pirates of the Caribbean.json";
+import PiratesData from "./json/Pirates of the Caribbean - He's a Pirate (3).json";
+import TakeFiveData from "./json/Take-Five-1.json";
+import WiiChannelsData from "./json/Wii Channels - Mii Channel.json";
 
 export const SongId = {
   ColtraneCountdown: "ColtraneCountdown",
   PiratesOfTheCaribbean: "PiratesOfTheCaribbean",
+  TakeFive: "TakeFive",
+  WillChannel: "WillChannel",
 } as const;
 
 // enums causing an issue, so dumb workaround
@@ -13,4 +17,6 @@ export type SongId = (typeof SongId)[keyof typeof SongId];
 export const SONG_LIST: Record<SongId, MidiSongJson> = {
   ColtraneCountdown: ColtraneData,
   PiratesOfTheCaribbean: PiratesData,
+  TakeFive: TakeFiveData,
+  WillChannel: WiiChannelsData,
 };
